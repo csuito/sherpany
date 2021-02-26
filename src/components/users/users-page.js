40f61213c3e36808b283a6b2e25a9dc50d419e73
Carlos Suito <suitoschacht@gmail.com>
2021-02-26 12:00:31 -0400

    retrieving users from api

--- src/components/users/users-page.js
+++ src/components/users/users-page.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import { useAxios } from '../../services/client'
 
 const UsersPage = () => {
+	const page = 1
+	const results = 50
+	const nat = 'ch,es,fr,gb'
+
+	const [{ data, loading, error, refetch }] = useAxios(
+		`?page=${page}&results=${results}&nat=${nat}`
+	)
+
+	console.log(data)
+
 	return (
 		<div>
-			<h1>Users</h1>
+			{loading && (
+				<div>
+					<p>Loading...</p>
+				</div>
+			)}
+
+			{error && (
+				<div>
+					<p>There was an error loading users data</p>
+					<button onClick={refetch}>Try again</button>
+				</div>
+			)}
+
+			<div>
+				{data && Array.isArray(data?.results) && data?.results.length >= 1 ? (
+					data.results.map((user) => (
+						<div>
+							<p>
+								{user.name.title} {user.name.first} {user.name.last}
+							</p>
+						</div>
+					))
+				) : (
+					<p>There are no users matching your query</p>
+				)}
+			</div>
 		</div>
 	)
 }
